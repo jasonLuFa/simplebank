a13167606f67c2d052240e21b18f45ce03fb4983
Jason Lu <52907691+jasonLuFa@users.noreply.github.com>
2023-05-02 15:25:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "run db migration"
 # shellcheck source=/dev/null
-. /app/app.env
+. /app/app.env # same as `source /app/app.env`
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
