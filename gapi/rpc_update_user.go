4361b145866545edad993396731aa045c6929205
jasonLuFa <a3558jason@gmail.com>
2023-05-22 09:07:32 +0800

    Ft/add grpc auth (#11)
    * customize the grpc response message
    
    * update db nullable argument
    
    * add auth in grpc and http gateway when update user

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -14,9 +14,18 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	authPayload, err := server.authorization(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	violations := validateUpdateUserRequest(req)
 	if violations != nil {
 		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	}
 
 	arg := db.UpdateUserParams{
